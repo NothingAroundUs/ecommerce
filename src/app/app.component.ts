b1f136ae53125696c434d812da40015501945b31
NothingAroundUs <husseintaha756@gmail.com>
2025-02-04 06:12:30 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CheckoutComponent } from "./checkout/checkout.component";
+import { CartComponent } from "./cart/cart.component";
+import { ProductComponent } from "./product/product.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
