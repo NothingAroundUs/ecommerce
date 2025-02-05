2a194f236143a39a246c97c41e06c5bc71de2c2f
NothingAroundUs <husseintaha756@gmail.com>
2025-02-05 04:57:45 -0800

    added API service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CheckoutComponent } from "./checkout/checkout.component";
 import { CartComponent } from "./cart/cart.component";
 import { ProductComponent } from "./product/product.component";
+import { NavComponent } from "./nav/nav.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CartComponent],
+  imports: [CartComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
